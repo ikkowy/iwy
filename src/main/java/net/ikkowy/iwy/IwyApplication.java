3515f2d63699a43a1481d047e8030aa82d6ef3b0
Vincent Westenberg <vincent.westenberg@posteo.de>
2024-11-01 04:57:29 +0100

    Initial commit

---
+++ src/main/java/net/ikkowy/iwy/IwyApplication.java
@@ -0,0 +1,13 @@
+package net.ikkowy.iwy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ikkowy/iwy/IwyApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ikkowy.iwy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
